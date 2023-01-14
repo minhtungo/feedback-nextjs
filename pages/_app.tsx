df0707417ea1dab1cc62ef1c546b5f89423edab6
Minh Tu Ngo <loducfc@gmail.com>
2023-01-14 18:35:39 -0500

    Set Up Chakra UI and Absolute Imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,26 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { AuthProvider } from './../lib/auth';
+import { AuthProvider } from '@/lib/auth';
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      html: {
+        minWidth: '360px',
+        scrollBehavior: 'smooth',
+      },
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
